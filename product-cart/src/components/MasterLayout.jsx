129cdb6bcc535f3676f2af6e9cfc93e8e145c59d
imtiaz-cnits <ia64744@gmail.com>
2024-03-28 04:11:49 +0600

    first commit

---
+++ product-cart/src/components/MasterLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppNavBar from "./AppNavBar.jsx";
+import Footer from "./Footer.jsx";
+
+const MasterLayout = (props) => {
+    return (
+        <div className="bg-light">
+            <AppNavBar/>
+            {props.children}
+            <Footer/>
+        </div>
+    );
+};
+
+export default MasterLayout;
---
+++ product-cart/src/utility/Helper.js
@@ -0,0 +1,10 @@
+class Helper {
+    static isLogin() {
+        return false;
+    }
+
+    static API_BASE = "https://cart-api.teamrabbil.com/api";
+
+}
+
+export default Helper
